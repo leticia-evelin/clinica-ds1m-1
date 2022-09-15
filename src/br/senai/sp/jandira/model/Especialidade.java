802b1104c98e4688131ac3b0dc7253cb8d67a2f9
CELSO MARCOS FURTADO <sn1006137@CFP127ALUNO.LOCAL>
2022-09-15 11:26:36 -0300

    Clinica 15-09 turma toda

---
+++ src/br/senai/sp/jandira/model/Especialidade.java
@@ -0,0 +1,16 @@
+package br.senai.sp.jandira.model;
+
+public class Especialidade {
+	
+	private String nome;
+	
+	// Métodos de acesso getters and setters
+	public void setNome(String novoNome) {
+		nome = novoNome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+	
+}
---
+++ src/br/senai/sp/jandira/model/PlanoDeSaude.java
@@ -0,0 +1,29 @@
+package br.senai.sp.jandira.model;
+
+public class PlanoDeSaude {
+	
+	private String operadora;
+	private String tipoDoPlano;
+	
+	// Métodos de acesso
+	public void setOperadora(String operadora) {
+		this.operadora = operadora;
+	}
+	
+	public String getOperadora() {
+		return this.operadora;
+	}
+	
+	public void setTipoDoPlano(String tipoDoPlano) {
+		this.tipoDoPlano = tipoDoPlano;
+	}
+	
+	public String getTipoDoPlano() {
+		return this.tipoDoPlano;
+	}
+	
+	public String getDadosDoPlano() {
+		return "Plano: " + this.operadora + ", " + this.tipoDoPlano;
+	}
+	
+}
